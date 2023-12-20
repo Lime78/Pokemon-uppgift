b101ef3018159fa60d4a27ec997ac575bc247e05
Liam Bard <liam.axel.bard@gmail.com>
2023-12-20 20:47:15 +0100

    massa gott

--- pokemon/uppgift/script.js
+++ pokemon/uppgift/script.js
@@ -1,25 +1,61 @@
-export const väljLag = document.querySelector("välj-lag");
+    document.addEventListener("DOMContentLoaded", function() {
+    const playButton = document.getElementById("start-knapp");
+    const pageToHide = document.getElementById("hide-on-play");
+    const vänsterBild = document.getElementById("vänster-bild");
+    const mittenBild = document.getElementById("mitten-bild");
+    const nedreBild = document.getElementById("nedre-bild");
+    const högerBild = document.getElementById("höger-bild");
+    const väljLag = document.querySelector("#välj-lag");
+    const pokemonBild = document.querySelector("#pokemon-bild");
+    const searchForm = document.querySelector("#searchInput")
   
-  
-  document.addEventListener("DOMContentLoaded", function() {
-  const playButton = document.getElementById("start-knapp");
-  const pageToHide = document.getElementById("hide-on-play");
-  const vänsterBild = document.getElementById("vänster-bild");
-  const mittenBild = document.getElementById("mitten-bild");
-  const nedreBild = document.getElementById("nedre-bild");
-  const högerBild = document.getElementById("höger-bild");
-  const väljLag = document.querySelector("välj-lag");
-  
-  playButton.addEventListener("click", function() {
+      playButton.addEventListener("click", function() {
       pageToHide.style.display = "none";
       vänsterBild.style.display = "none";
       mittenBild.style.display = "none";
       nedreBild.style.display = "none";
       högerBild.style.display = "none"
-      väljLag.style.display = "none";
-  });
-});
+      playButton.style.display = "none";
+      pokemonBild.style.display = "none";
+    });
+  
+    document.getElementById("searchForm").addEventListener("submit", function(event) {
+      event.preventDefault(); 
+      var searchQuery = document.getElementById("searchInput").value;
+      console.log("Sökning: ", searchQuery);
+    });
+  });  //sökväg tror ja
 
-pickContainer.addEventListener("click"), function() {
-  väljLag.style.display = "none";
+function toggleVisibility() {
+  var boxarElement = document.querySelector(".boxar");
+  var computedStyle = window.getComputedStyle(boxarElement);
+
+  if (computedStyle.display === "none") {
+    boxarElement.style.display = "grid";
+  } else {
+    boxarElement.style.display = "none";
+  }
 }
+
+// document.getElementById("searchForm").addEventListener("submit", function(event) {
+//   event.preventDefault(); 
+
+  
+//   var searchQuery = document.getElementById("searchInput").value;
+
+ 
+//   console.log("Sökning: ", searchQuery);
+// }); sökvägen
+
+
+
+// function toggleVisibility() {
+//   var topBildElement = document.querySelector("bild");
+//   var computedStyle = window.getComputedStyle(topBildElement);
+
+//   if (computedStyle.display === "none") {
+//     topBildElement.style.display = "flex";
+//   } else {
+//     topBildElement.style.display = "none";
+//   }
+// }
