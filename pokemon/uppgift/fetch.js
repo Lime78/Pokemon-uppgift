e1904b33032311f4f756f6566a27d3e235089f06
Liam Bard <liam.axel.bard@gmail.com>
2023-12-22 15:55:56 +0100

    knapp kod osv

--- pokemon/uppgift/fetch.js
+++ pokemon/uppgift/fetch.js
@@ -1,54 +1,61 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const pokedex = document.getElementById('pokedex');
-    const searchInput = document.getElementById('searchInput');
-    const searchButton = document.getElementById('searchButton');
+// document.addEventListener("DOMContentLoaded", function() {
+//     const pokedex = document.getElementById('pokedex');
+//     const searchInput = document.getElementById('searchInput');
+//     const searchButton = document.getElementById('searchButton');
 
-    let allPokemon = []; // Store all fetched Pokémon
+//     let allPokemon = []; // Store all fetched Pokémon
 
-    const fetchPokemon = () => {
-        const promises = [];
-        for (let i = 1; i <= 150; i++) {
-            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
-            promises.push(fetch(url).then((res) => res.json()));
-        }
-        Promise.all(promises).then((results) => {
-            allPokemon = results.map((result) => ({
-                name: result.name,
-                image: result.sprites['front_default'],
-                type: result.types.map((type) => type.type.name).join(', '),
-                id: result.id
-            }));
-            displayPokemon(allPokemon);
-        });
-    };
+//     const fetchPokemon = () => {
+//         const promises = [];
+//         for (let i = 1; i <= 150; i++) {
+//             const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+//             promises.push(fetch(url).then((res) => res.json()));
+//         }
+//         Promise.all(promises).then((results) => {
+//             allPokemon = results.map((result) => ({
+//                 name: result.name,
+//                 image: result.sprites['front_default'],
+//                 type: result.types.map((type) => type.type.name).join(', '),
+//                 id: result.id
+//             }));
+//             displayPokemon(allPokemon);
+//         });
+//     };
 
-    const displayPokemon = (pokemon) => {
-        const pokemonHTMLString = pokemon
-            .map(
-                (pokeman) => `
-                    <li class="card">
-                        <img class="card-image" src="${pokeman.image}"/>
-                        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
-                        <p class="card-subtitle">Type: ${pokeman.type}</p>
-                    </li>
-                `
-            )
-            .join('');
-        pokedex.innerHTML = pokemonHTMLString;
-    };
 
-    const filterPokemon = (searchTerm) => {
-        const filteredPokemon = allPokemon.filter((pokemon) =>
-            pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        displayPokemon(filteredPokemon);
-    };
+//     //mina klasser
+//     const displayPokemon = (pokemon) => {
+//         const pokemonHTMLString = pokemon
+//             .map(
+//                 (pokeman) => `
+//                     <li class="card">
+//                         <img class="card-image" src="${pokeman.image}"/>
+//                         <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+//                         <p class="card-subtitle">Type: ${pokeman.type}</p>
+//                     </li>
+//                 `
+//             )
+//             .join('');
+//         pokedex.innerHTML = pokemonHTMLString;
+//     };
 
-    searchButton.addEventListener('click', () => {
-        const searchTerm = searchInput.value.trim();
-        filterPokemon(searchTerm);
-    });
+//     fetchPokemon();
+// });
 
-    // Fetch Pokémon data
-    fetchPokemon();
-});
+
+
+   //knappar till pokemonsen
+// <button id="my-pokemons-${index}">Move to Class 1</button>
+                        // <button id="reserv-pokemons-${index}">Move to Class 2</button>
+
+                        // pokemon.forEach((pokeman, index) => {
+    //     const team-pokemons = document.getElementById(`my-pokemons-${index}`);
+    //     const reserv-pokemons = document.getElementById(`reserv-pokemons-${index}`);
+    
+    //     teamPokemons.addEventListener('click', function() {
+          
+    //     });
+    
+    //     reservPokemons.addEventListener('click', function() {
+          
+    //     });
--- pokemon/uppgift/script.js
+++ pokemon/uppgift/script.js
@@ -1,27 +1,26 @@
-    document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function() {
     const playButton = document.getElementById("start-knapp");
     const pageToHide = document.getElementById("hide-on-play");
     const vänsterBild = document.getElementById("vänster-bild");
     const mittenBild = document.getElementById("mitten-bild");
     const nedreBild = document.getElementById("nedre-bild");
     const högerBild = document.getElementById("höger-bild");
-    const väljLag = document.querySelector("#välj-lag");
     const pokemonBild = document.querySelector("#pokemon-bild");
-    const searchInput = document.querySelector("#searchInput")
-  
-
-    //gömmer allt för play
-      playButton.addEventListener("click", function() {
-      pageToHide.style.display = "none";
-      vänsterBild.style.display = "none";
-      mittenBild.style.display = "none";
-      nedreBild.style.display = "none";
-      högerBild.style.display = "none"
-      playButton.style.display = "none";
-      pokemonBild.style.display = "none";
-      lagKnapp.style.display = "none";
-    });
     const lagKnapp = document.getElementById("lag-knapp");
+    const teamPokemons = document.getElementById("team-pokemons");
+
+    //gömmer allt för play  //gör en container för start en för team osv
+        playButton.addEventListener("click", function() {
+        pageToHide.style.display = "none";
+        vänsterBild.style.display = "none";
+        mittenBild.style.display = "none";
+        nedreBild.style.display = "none";
+        högerBild.style.display = "none"
+        playButton.style.display = "none";
+        pokemonBild.style.display = "none";
+        lagKnapp.style.marginTop = "-580px";
+        teamPokemons.style.display = "none";
+    });
 
     lagKnapp.addEventListener("click", function() {
         const boxarElement = document.querySelector(".boxar");
@@ -33,14 +32,16 @@
         const playButton = document.getElementById("start-knapp");
         const pokemonBild = document.getElementById("pokemon-bild");
         const searchInput = document.getElementById("searchInput");
+        
 
         // gömmer allt för myTeam
         [boxarElement, pageToHide, vänsterBild, mittenBild, nedreBild, högerBild, playButton, pokemonBild, searchInput]
             .forEach(element => element.style.display = "none");
     });
-
 });
 
+
+// sökrutan för api synlighet
 function toggleVisibility() {
     var boxarElement = document.querySelector(".boxar");
     var computedStyle = window.getComputedStyle(boxarElement);
@@ -51,3 +52,84 @@ function toggleVisibility() {
         boxarElement.style.display = "none";
     }
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+    const pokedex = document.getElementById('pokedex');
+    const searchInput = document.getElementById('searchInput');
+    const searchButton = document.getElementById('searchButton');
+
+    let allPokemon = []; // alla lagrade pokemons
+
+    const fetchPokemon = (pokemonName) => {
+        const promises = [];
+            
+        for (let i = 1; i <= 10; i++) { //lägg till ett annat api, för att få lista på alla pokemons url
+            const url = `https://pokeapi.co/api/v2/pokemon/${i}`;
+            promises.push(fetch(url).then((res) => res.json()));
+        }
+        Promise.all(promises).then((results) => {
+            allPokemon = results.map((result) => ({
+                name: result.name,
+                image: result.sprites['front_default'],
+                type: result.types.map((type) => type.type.name).join(', '),
+                id: result.id
+            }));
+            displayPokemon(allPokemon);
+
+            const lagKnappContainer = document.getElementById("lag-knapp-container");
+
+            pokedex.addEventListener("click", function(event) {
+                if (event.target.classList.contains("team-pokemons")) {
+                    const selectedPokemon = event.target.closest(".card");
+                    const clonedPokemon = selectedPokemon.cloneNode(true);
+            
+                    // Ta bort "ADD TO TEAM" -knappen från den klonade Pokémon för att förhindra oändlig rekursion
+                    const addToTeamButton = clonedPokemon.querySelector(".team-pokemons");
+                    addToTeamButton.remove();
+            
+                    if (lagKnappContainer) {
+                        lagKnappContainer.appendChild(clonedPokemon);
+                    } else {
+                        console.error('lagKnappContainer is null');
+                    }
+                }
+            });
+        });
+    };
+
+
+    //mina klasser, ändra namn till pokemonList, göra en lista med dom element, creatElement, innerHtml plocka ut html genom quareselector
+    const displayPokemon = (pokemon) => {
+        const pokemonHTMLString = pokemon
+            .map(
+                (pokeman) => `
+                    <li class="card">
+                        <img class="card-image" src="${pokeman.image}"/>
+                        <h2 class="card-title">${pokeman.id}. ${pokeman.name}</h2>
+                        <button class="team-pokemons">ADD TO TEAM</button>
+                    </li>
+                `
+            )
+            .join('');
+        pokedex.innerHTML = pokemonHTMLString;
+    };
+
+    // sök i api
+    const searchForm = document.getElementById('searchForm');
+
+    searchForm.addEventListener('submit', function(event) {
+      event.preventDefault(); 
+      const searchTerm = searchInput.value.toLowerCase();
+      const filteredPokemon = allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+      displayPokemon(filteredPokemon);
+    });
+
+    fetchPokemon();
+    searchButton.addEventListener('click', function() {
+        const searchTerm = searchInput.value.toLowerCase();
+        const filteredPokemon = allPokemon.filter(pokemon => pokemon.name.toLowerCase().includes(searchTerm));
+        displayPokemon(filteredPokemon);
+    });   
+          
+        });
+    
